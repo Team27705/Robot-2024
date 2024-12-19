deedf4918cbe9fb93d900efd3f8a1757ada494bf
Rain Chen <rainc8@nycstudents.net>
2024-12-19 16:58:33 -0500

    added motors to Drivetrain

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Drivetrain.java
@@ -1,8 +1,27 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.arcrobotics.ftclib.drivebase.MecanumDrive;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+
+
 
 public class Drivetrain {
-    DcMotor motorOne = new
+    Motor fL = new Motor(hardwareMap, "frontLeftMotor");
+    Motor bL = new Motor(hardwareMap, "backLeftMotor");
+    Motor fR = new Motor(hardwareMap, "frontRightMotor");
+    Motor bR = new Motor(hardwareMap, "backRightMotor");
+
+    DcMotor frontLeft = fL.motor;
+    DcMotor backLeft = bL.motor;
+    DcMotor frontRight = fR.motor;
+    DcMotor backRight = bR.motor;
+
+    MecanumDrive mecanum = new MecanumDrive(fL, bL,
+            fR, bR);
+
+
 }
