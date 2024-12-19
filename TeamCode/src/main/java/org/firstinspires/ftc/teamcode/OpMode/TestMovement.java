4c0be53a15ced38b550035b2c2c6ba1fc1a57bd2
rainy1221 <rain102208@gmail.com>
2024-12-19 00:16:57 -0500

    Added Subsystems, a file for Operational Modes, Main file might be useless due to the nature of the Robot Class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/TestMovement.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.OpMode;
+
+import Subsystems.Drivetrain;
+
+public class TestMovement extends Drivetrain {
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Drivetrain.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class Drivetrain {
+
+}
